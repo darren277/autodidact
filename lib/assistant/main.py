c3d4d066b69b5813ae6752659dc0a57c822d30c4
darren277 <darren277@yahoo.com>
2024-11-08 07:16:32 -0800

    Refactoring into testable class.

--- lib/assistant/main.py
+++ lib/assistant/main.py
@@ -3,34 +3,42 @@ from lib.assistant.initialize import logger
 from lib.assistant.process_run_events import process_run_events
 from lib.assistant.start_assistant_run import start_assistant_run
 
-full_message = ""
-
 DEFAULT_PROMPT = "Add 1234 and 5678 then multiply by 2."
 DEFAULT_ASSISTANT_ID = "asst_X0dIT6aOTHFQgJNE923sjv8E"
 
 
-def front_end_callback(message):
-    # Replace this with your actual front-end streaming implementation
-    logger.debug(f"Assistant: {message}")
-    global full_message
+class AssistantHandler:
+    def __init__(self, prompt: str, assistant_id: str, tools: [dict] = None):
+        self.prompt = prompt
+        self.assistant_id = assistant_id
+        self.tools = tools
 
-    full_message += message[0].text.value
+        self._app = None
+        self._logger = logger
 
-def handle_assistant_response(app, question: str, py_thread_id):
-    with app.app_context():
-        #asm = AssistantStateMachine(py_thread_id, question, tools)
-        #asm.start()
-        ...
+        self._full_message = ""
 
-def main(prompt: str, assistant_id: str, tools: [dict] = None):
-    from lib.tools.arithmetic import tools as arithmetic_tools
+    def initialize_app(self, app):
+        self._app = app
 
-    tools = tools or arithmetic_tools
-    event_stream, thread_id = start_assistant_run(prompt, assistant_id, tools)
-    process_run_events(event_stream, thread_id, front_end_callback)
+    def front_end_callback(self, message):
+        # Replace this with your actual front-end streaming implementation
+        logger.debug(f"Assistant: {message}")
+        self._full_message += message[0].text.value
 
-    logger.debug(f"Full message: {full_message}")
+        #def handle_assistant_response(app, question: str, py_thread_id):
+        if self._app:
+            with self._app.app_context():
+                #asm = AssistantStateMachine(py_thread_id, question, tools)
+                #asm.start()
+                ...
 
+    def run(self):
+        from lib.tools.arithmetic import tools as arithmetic_tools
 
-if __name__ == "__main__":
-    main(prompt=DEFAULT_PROMPT, assistant_id=DEFAULT_ASSISTANT_ID)
+        tools = self.tools or arithmetic_tools
+        event_stream, thread_id = start_assistant_run(self.prompt, self.assistant_id, tools)
+        process_run_events(event_stream, thread_id, self.front_end_callback)
+
+        self._logger.debug(f"Full message: {self._full_message}")
+
