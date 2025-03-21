f6a18e6bf8c6e87505189619a853a64250de41e4
darren277 <darren277@yahoo.com>
2025-03-21 07:04:19 -0700

    Main TTS class.

---
+++ lib/tts/main.py
@@ -0,0 +1,31 @@
+""""""
+from openai import AsyncOpenAI
+
+from lib.tts.personalities import voices
+
+openai = AsyncOpenAI()
+
+
+class TTS:
+    def __init__(self, model: str, voice: str, instructions: str):
+        if model not in ["gpt-4o-mini-tts"]:
+            raise ValueError(f"Model '{model}' is not available for TTS.")
+        self.model = model
+
+        if voice not in voices:
+            raise ValueError(f"Voice '{voice}' is not available for model '{model}'.")
+        self.voice = voice
+
+        self.instructions = instructions
+
+    async def speak(self, message: str) -> None:
+        async with openai.audio.speech.with_streaming_response.create(
+            model=self.model,
+            voice=self.voice,
+            input=message,
+            instructions=self.instructions,
+            response_format="pcm",
+        ) as response:
+            #await LocalAudioPlayer().play(response)
+            return response
+
