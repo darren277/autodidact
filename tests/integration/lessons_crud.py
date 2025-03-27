7f7d54b3778548535cc60443ad257e0807a856f7
darren277 <darren277@yahoo.com>
2025-03-27 11:24:43 -0700

    Added test for fetching a specific lesson.

--- tests/integration/lessons_crud.py
+++ tests/integration/lessons_crud.py
@@ -23,6 +23,13 @@ def test_get_lessons():
     print(response.json())
 
 
+def test_get_specific_lesson():
+    response = requests.get(f'http://localhost:{PORT}/api/lessons/1')
+
+    print(response.status_code)
+    print(response.json())
+
+
 def test_update_lesson():
     data = {
         "title": "Updated Test Lesson",
@@ -46,6 +53,7 @@ def test_delete_lesson():
 if __name__ == '__main__':
     test_create_lesson()
     test_get_lessons()
+    test_get_specific_lesson()
     test_update_lesson()
     test_delete_lesson()
 
